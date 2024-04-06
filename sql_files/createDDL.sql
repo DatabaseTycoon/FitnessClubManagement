3bc654016dfececb08c389ba4de3875fdf336cf4
orselane <77144245+orselane@users.noreply.github.com>
2024-04-06 17:47:01 -0400

    Changed primary keys to use serial instead of int.
    - Allows us to ignore the primary key field when doing insertions

--- sql_files/createDDL.sql
+++ sql_files/createDDL.sql
@@ -1,22 +1,22 @@
 -- DDL file for FitnessClubManagement
 -- Can't name admin (keyword)
 CREATE TABLE Administrator (
-	adminID int PRIMARY KEY
+	adminID serial PRIMARY KEY
 );
 
 CREATE TABLE Trainer (
-	trainerID int PRIMARY KEY
+	trainerID serial PRIMARY KEY
 );
 
 -- Name is a keyword
 CREATE TABLE PersonName (
-	nameID int PRIMARY KEY,
+	nameID serial PRIMARY KEY,
 	firstName varchar(20) NOT NULL,
 	lastName varchar(20) NOT NULL
 );
 
 CREATE TABLE Staff (
-	staffID int PRIMARY KEY,
+	staffID serial PRIMARY KEY,
 	nameID int REFERENCES PersonName (nameID),
 	email varchar(60) NOT NULL,
 	phoneNumber varchar(11)
@@ -35,11 +35,11 @@ CREATE TABLE IsTrainer (
 );
 
 CREATE TABLE Room (
-	roomID int PRIMARY KEY
+	roomID serial PRIMARY KEY
 );
 
 CREATE TABLE Equipment (
-	equipmentID int PRIMARY KEY,
+	equipmentID serial PRIMARY KEY,
 	roomID int REFERENCES Room (roomID),
 	status varchar(15),
 	equipmentName varchar(30) -- Name is a keyword
@@ -47,11 +47,11 @@ CREATE TABLE Equipment (
 
 -- Class is a keyword
 CREATE TABLE GymClass (
-	classID int PRIMARY KEY,
-	roomID int REFERENCES Room (roomID), 
+	classID serial PRIMARY KEY,
+	roomID int REFERENCES Room (roomID),
 	startDate timestamp NOT NULL,
 	endDate timestamp NOT NULL,
-	capacity int 
+	capacity int
 );
 
 
@@ -64,7 +64,7 @@ CREATE TABLE Runs (
 
 -- Should statistic be "value", and "type" where type is a string element of {HeartRate, BloodPressure, Weight, Height}?
 CREATE TABLE Statistic (
-	statID int PRIMARY KEY,
+	statID serial PRIMARY KEY,
 	HeartRate int,
 	BloodPressure int,
 	Weight int,
@@ -72,16 +72,16 @@ CREATE TABLE Statistic (
 	statDate date NOT NULL
 );
 
--- What if we reused statistic to be able to check any metric? I.e. target metric? Statistic could contain metric actually 
+-- What if we reused statistic to be able to check any metric? I.e. target metric? Statistic could contain metric actually
 CREATE TABLE FitnessGoal (
-	goalID int PRIMARY KEY,
+	goalID serial PRIMARY KEY,
 	isAchieved bool NOT NULL,
 	targetDate date NOT NULL,
 	targetWeight int NOT NULL
 );
 
 CREATE TABLE EmergencyContact (
-	emergencyContactID int PRIMARY KEY,
+	emergencyContactID serial PRIMARY KEY,
 	nameID int REFERENCES PersonName (nameID),
 	dateOfBirth date,
 	-- Contact info reuse with staff (maybe that's okay though)
@@ -90,7 +90,7 @@ CREATE TABLE EmergencyContact (
 );
 
 CREATE TABLE PersonalInfo (
-	personalInfoID int PRIMARY KEY,
+	personalInfoID serial PRIMARY KEY,
 	nameID int REFERENCES PersonName (nameID),
 	emergencyContactID int REFERENCES EmergencyContact (emergencyContactID),
 	-- Why store DoB?... -> What if name became contact info (used by three tables now): FName, LName, email, number.
@@ -101,7 +101,7 @@ CREATE TABLE PersonalInfo (
 );
 
 CREATE TABLE BillingInfo (
-	billingInfoID int PRIMARY KEY,
+	billingInfoID serial PRIMARY KEY,
 	billingAddress varchar(60) NOT NULL,
 	memEndDate date NOT NULL,
 	creditCardNumber int NOT NULL,
@@ -111,10 +111,9 @@ CREATE TABLE BillingInfo (
 
 -- Member is a keyword. Not putting phone number + e-mail since it's already in personal info.
 CREATE TABLE MemberInfo(
-	memberInfo int PRIMARY KEY,
+	memberInfo serial PRIMARY KEY,
 	personalInfoID int REFERENCES PersonalInfo (personalInfoID),
 	billingInfoID int REFERENCES BillingInfo (billingInfoID),
 	statID int REFERENCES Statistic (statID),
 	goalID int REFERENCES FitnessGoal (goalID)
 );
-
