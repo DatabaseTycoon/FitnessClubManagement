b70965f36987eb79fd31c939656419859dade196
Mayukh <mayukh.gautam@gmail.com>
2024-04-06 22:47:29 -0400

    Added Billing. Fixed application loops

--- src/admin.py
+++ src/admin.py
@@ -10,26 +10,29 @@ class Admin:
         self.db = db
 
     def show_main_menu(self):
-        print("\n" * 50)
-        main_menu_options = ["Manage Booked Rooms", "Update Class Schedule", "Equipment Maintenance Monitoring",
-                             "Billment And Payment"]
-        selected_option = get_option_input(main_menu_options, "Admin Menu", 2)
+        while True:
+            print("\n" * 50)
+            main_menu_options = ["Manage Booked Rooms", "Update Class Schedule", "Equipment Maintenance Monitoring",
+                                "Billment And Payment", "Back"]
+            selected_option = get_option_input(main_menu_options, "Admin Menu", 2)
+            if selected_option == 0:
+                print("Selected: Book class (manage booked rooms)")
+                capacity = get_int_input(" > Please enter a capacity for the class ", 0, 20)
+                start_time = get_datetime_input(" > Please enter a start time for the class")
+                end_time = get_datetime_input(" > Please enter an end time for the class")
+                equipment_name_in = input("> Please enter a comma seperated list of required equipment for the class "
+                                        "(leave empty if N/A): ")
+                equipment_names = [name.strip() for name in equipment_name_in.split(",")]
+                self.book_class(capacity, start_time, end_time, equipment_names)
+            elif selected_option == 1:
+                print("Selected: Update Class Schedule")
+            elif selected_option == 2:
+                self.equipment_maintenance_monitoring()
+            elif selected_option == 3:
+                self.billment_and_payment()
+            elif selected_option == 4:
+                return
 
-        if selected_option == 0:
-            print("Selected: Book class (manage booked rooms)")
-            capacity = get_int_input(" > Please enter a capacity for the class ", 0, 20)
-            start_time = get_datetime_input(" > Please enter a start time for the class")
-            end_time = get_datetime_input(" > Please enter an end time for the class")
-            equipment_name_in = input("> Please enter a comma seperated list of required equipment for the class "
-                                      "(leave empty if N/A): ")
-            equipment_names = [name.strip() for name in equipment_name_in.split(",")]
-            self.book_class(capacity, start_time, end_time, equipment_names)
-        elif selected_option == 1:
-            print("Selected: Update Class Schedule")
-        elif selected_option == 2:
-            self.equipment_maintenance_monitoring()
-        elif selected_option == 3:
-            self.billment_and_payment()
 
     def equipment_maintenance_monitoring(self):
         equipment_data = self.db.select(["*"], "equipment", select_options={})
@@ -45,26 +48,24 @@ class Admin:
             for equipment_id, room_id, status, equipment_name in filtered_equipment_data:
                 print("{:<10} {:<15} {:<30} {:<10}".format(equipment_id, status, equipment_name, room_id))
 
-            option_list = ["Quit", "Sort by room", "Sort by status", "Filter for Working", "Filter for Maintenance",
-                           "Filter for Out of Order"]
+            option_list = ["Sort by room", "Sort by status", "Filter for Working", "Filter for Maintenance",
+                           "Filter for Out of Order", "Back"]
             option = get_option_input(option_list)
 
             if option == 0:
-                break
-            if option == 1:
                 filtered_equipment_data = equipment_data
                 filtered_equipment_data.sort(key=lambda e: e[1])
-            elif option == 2:
+            elif option == 1:
                 filtered_equipment_data = equipment_data
                 filtered_equipment_data.sort(key=lambda e: e[2])
-            elif option == 3:
+            elif option == 2:
                 filtered_equipment_data = list(filter(lambda e: e[2] == 'Working', equipment_data))
-            elif option == 4:
+            elif option == 3:
                 filtered_equipment_data = list(filter(lambda e: e[2] == 'Maintenance', equipment_data))
-            elif option == 5:
+            elif option == 4:
                 filtered_equipment_data = list(filter(lambda e: e[2] == 'Out of Order', equipment_data))
-
-        self.show_main_menu()
+            elif option == 5:
+                return
 
 
     def billment_and_payment(self):
@@ -78,25 +79,26 @@ class Admin:
             print("\n\t\tBILLING MENU\n")
             print("\n\t\tMEMBERS LIST\n")
 
-            print("{:<10} {:<15} {:<30} {:<10}".format("MemberID", "First Name", "Last Name", "Email"))
+            print("{:<10} {:<20} {:<20} {:<10}".format("MemberID", "First Name", "Last Name", "Email"))
             for member in members:
                 member_pinfo = list(filter(lambda personal: personal[0] == member[1], p_info))[0]
                 member_contact = list(filter(lambda contact: contact[0] == member_pinfo[1], c_info))[0]
-                print("{:<10} {:<15} {:<30} {:<10}".format(member[0], member_contact[1], member_contact[2], member_contact[3]))
+                print("{:<10} {:<20} {:<20} {:<10}".format(member[0], member_contact[1], member_contact[2], member_contact[3]))
 
             error = False
-            print("- Q to quit -")
+            print("\n- B to go Back -")
             ans = input("\nMemberID > ")
-            if ans == 'Q':
+            if ans == 'B':
                 return
             
             if not ans.isnumeric():
                 error = True
             else:    
-                chosen_member = list(filter(lambda m: m[0] == int(ans), members))[0]
+                chosen_member = list(filter(lambda m: m[0] == int(ans), members))
                 if not chosen_member:
                     error = True
                 else:
+                    chosen_member = chosen_member[0]
                     member_binfo = list(filter(lambda billing: billing[0] == chosen_member[2], b_info))[0]
                     self._show_billing(member_binfo)
 
@@ -109,9 +111,16 @@ class Admin:
         print("\n" * 50)
         billing_info = billing_info[1:]
 
-        print("\n\t\tBILLING INFO\n")
+        adr = billing_info[0]
+        join = str(billing_info[1])
+        cnum_string = str(billing_info[2])
+        censor_card_num = "***.." + cnum_string[len(cnum_string) - 3:]
+        censor_exp_date = "****-**-**"
+        censor_cvv = "***"
+
+        print("\n\t\tBILLING INFO --- BILL THIS MEMBER ?\n")
         print("{:<30} {:<15} {:<15} {:<15} {:<10}".format("Address", "Join Date", "Card Num", "Exp", "CVV"))
-        print("{:<30} {:<15} {:<15} {:<15} {:<10}".format(billing_info[0], str(billing_info[1]), billing_info[2], str(billing_info[3]), billing_info[4]))
+        print("{:<30} {:<15} {:<15} {:<15} {:<10}".format(adr, join, censor_card_num, censor_exp_date, censor_cvv))
 
         options = ["YES", "NO"]
         option = get_option_input(options)
@@ -129,7 +138,8 @@ class Admin:
         available_rooms = self._get_available_rooms(start_time, end_time, equipment_names)
 
         if not available_rooms:
-            print('No rooms available for booking')
+            print('\n--No rooms available for booking--\n')
+            time.sleep(2)
             return
 
         # Create a new class
@@ -137,7 +147,8 @@ class Admin:
                             [available_rooms.pop()[0], start_time, end_time, class_capacity],
                             ['roomid', 'startdate', 'enddate', 'capacity'])
 
-        print('Booked class.')
+        print('\n--Booked class--\n')
+        time.sleep(2)
 
     def _get_bookings(self, time_start, time_end) -> set[tuple[str]]:
         def is_before(s1, e1, sTest):
