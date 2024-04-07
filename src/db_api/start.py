ff065b4e5f5d7954a87681345cd4d754baa931cc
Mayukh <mayukh.gautam@gmail.com>
2024-04-07 15:36:07 -0400

    Added Update Method

--- src/db_api/start.py
+++ src/db_api/start.py
@@ -8,27 +8,30 @@ config = dotenv_values(__env_path)
 
 def main(db: Database):
 
-    ins_res = db.insert_into("members", rows=["name"], values=["Hello1"])
-    ins_res = db.insert_into("members", rows=["name"], values=["Hello2"])
-    ins_res = db.insert_into("members", rows=["name"], values=["Hello3"])
-    ins_res = db.insert_into("members", rows=["name"], values=["Hello4"])
-    ins_res = db.insert_into("members", rows=["name"], values=["Hello5"])
-    ins_res = db.insert_into("members", rows=["name"], values=["Hello5"])
-    ins_res = db.insert_into("members", rows=["name"], values=["Hello5"])
-    ins_res = db.insert_into("members", rows=["name"], values=["Hello5"])
+    ins_res = db.insert_into("test_table", rows=["name"], values=["Hello1"])
+    ins_res = db.insert_into("test_table", rows=["name"], values=["Hello2"])
+    ins_res = db.insert_into("test_table", rows=["name"], values=["Hello3"])
+    ins_res = db.insert_into("test_table", rows=["name"], values=["Hello4"])
+    ins_res = db.insert_into("test_table", rows=["name"], values=["Hello5"])
+    ins_res = db.insert_into("test_table", rows=["name"], values=["Hello5"])
+    ins_res = db.insert_into("test_table", rows=["name"], values=["Hello5"])
+    ins_res = db.insert_into("test_table", rows=["name"], values=["Hello5"])
 
-    del_res = db.delete_from("members", {"operation": "=", "rowA": "name", "rowB": "Hello5"})
+    # del_res = db.delete_from("test_table", {"operation": "=", "rowA": "name", "rowB": "Hello5"})
+    del_res = None
 
-    sel_res = db.select(["*"], "members", select_options={
-        "WHERE": {"operation": "<=", "rowA": "memberid", "rowB": "2"}
+    sel_res = db.select(["*"], "test_table", select_options={
+        "WHERE": {"operation": "<=", "rowA": "id", "rowB": "2"}
     })
 
-    sel_res_or = db.select_with_or(["*"], "members", 
-                                   {"operation": "<=", "rowA": "memberid", "rowB": "2"},
-                                   {"operation": ">=", "rowA": "memberid", "rowB": "3"},
-                                   {"operation": "=", "rowA": "memberid", "rowB": "4"})
+    sel_res_or = db.select_with_or(["*"], "test_table", 
+                                   {"operation": "<=", "rowA": "id", "rowB": "2"},
+                                   {"operation": ">=", "rowA": "id", "rowB": "3"},
+                                   {"operation": "=", "rowA": "id", "rowB": "4"})
+    
+    upd_res = db.update("test_table", [("name", "henry"), ("house", "there")], {"operation": "=", "rowA": "name", "rowB": "Hello5"})
 
-    print(sel_res, sel_res_or, del_res, ins_res)
+    print(sel_res, sel_res_or, del_res, ins_res, upd_res)
     exit(0)
 
 
